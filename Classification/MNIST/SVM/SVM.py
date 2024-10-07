113ca12a4770c1465e8fabf1cdf484ad3d7d1053
MohammadvHossein <habibpour.programming@gmail.com>
2024-10-07 12:43:50 +0330

    update svm and add gridSearch

--- Classification/MNIST/SVM/SVM.py
+++ Classification/MNIST/SVM/SVM.py
@@ -1,5 +1,5 @@
 from sklearn import datasets
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVC
 from sklearn.metrics import classification_report, confusion_matrix
@@ -20,11 +20,29 @@ X_test = scaler.transform(X_test)
 pickle.dump(scaler, open("standardScaler.pkl", 'wb'))
 
 clf = SVC()
-clf.fit(X_train, y_train)
 
-with open("SVM.pkl", 'wb') as model_file:
-    pickle.dump(clf, model_file)
+param_grid = {
+    'C': [0.1, 1, 10, 100],
+    'gamma': ['scale', 'auto', 0.001, 0.01, 0.1],
+    'kernel': ['linear', 'rbf', 'poly']
+}
 
-y_pred = clf.predict(X_test)
+grid_search = GridSearchCV(estimator=clf,
+                           param_grid=param_grid,
+                           scoring='accuracy',
+                           cv=5,
+                           verbose=2,
+                           n_jobs=-1)
+
+grid_search.fit(X_train, y_train)
+
+print(f"Best parameters: {grid_search.best_params_}")
+
+best_clf = grid_search.best_estimator_
+y_pred = best_clf.predict(X_test)
+
+with open("SVM_best.pkl", 'wb') as model_file:
+    pickle.dump(best_clf, model_file)
+
 print(classification_report(y_test, y_pred))
 print(confusion_matrix(y_test, y_pred))
