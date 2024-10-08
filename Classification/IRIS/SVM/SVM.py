71d96d62293a78058c2317c416382585661a0810
MohammadvHossein <habibpour.programming@gmail.com>
2024-10-08 15:04:49 +0330

    add svm to iris

---
+++ Classification/IRIS/SVM/SVM.py
@@ -0,0 +1,48 @@
+from sklearn import datasets
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
+from sklearn.metrics import classification_report, confusion_matrix
+import pickle
+
+data = datasets.load_iris()
+X = data.data
+y = data.target
+print(f"Data shape: {X.shape}, Target shape: {y.shape}")
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42)
+
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+pickle.dump(scaler, open("standardScaler.pkl", 'wb'))
+
+clf = SVC()
+
+param_grid = {
+    'C': [0.1, 1, 10, 100],
+    'gamma': ['scale', 'auto', 0.001, 0.01, 0.1],
+    'kernel': ['linear', 'rbf', 'poly']
+}
+
+grid_search = GridSearchCV(estimator=clf,
+                           param_grid=param_grid,
+                           scoring='accuracy',
+                           cv=5,
+                           verbose=2,
+                           n_jobs=-1)
+
+grid_search.fit(X_train, y_train)
+
+print(f"Best parameters: {grid_search.best_params_}")
+
+best_clf = grid_search.best_estimator_
+y_pred = best_clf.predict(X_test)
+
+with open("SVM_best.pkl", 'wb') as model_file:
+    pickle.dump(best_clf, model_file)
+
+print(classification_report(y_test, y_pred))
+print(confusion_matrix(y_test, y_pred))
